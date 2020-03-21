0d9465f30900d42989ce9b988c2c1833ed7129f1
Barış Ertaş <ertasbariis@gmail.com>
2020-03-22 00:45:01 +0300

    Update auto_login.py

--- auto_login.py
+++ auto_login.py
@@ -8,9 +8,14 @@ from selenium.webdriver.common.by import By
 
 mail = "*" #e-mail address here
 mailPw = "*" #e-mail password here
+
 driver = webdriver.Chrome('/Users/barisertas/Downloads/chromedriver-2') #path of the chrome-driver
+#you can also use safari or other browsers. -> webdriver.Safari(), webdriver.Firefox()
+
+
 url = "https://webmail.bilkent.edu.tr/" # you replace any url you want.
-driver.get(url) #opens the url
+
+driver.get(url) #opens the browser and directs you to url
 
 #html tags and getting them
 pw = '[id^="LoginForm-"]'
@@ -18,5 +23,6 @@ driver.find_element_by_id("rcmloginuser").send_keys(mail)
 pas = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, pw)))
 pas.send_keys(mailPw)
 button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID,"rcmloginsubmit")))
+
 button.click() #triggers login button
 
